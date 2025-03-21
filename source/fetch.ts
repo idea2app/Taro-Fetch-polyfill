a7e9ffdae7afec13d390afd05a78e1f8c4917a80
TechQuery <shiy2008@gmail.com>
2025-03-22 03:08:51 +0800

    [fix] Content Type of Taro Request & Response

--- source/fetch.ts
+++ source/fetch.ts
@@ -7,22 +7,34 @@ export async function fetch(input: RequestInfo | URL, init?: RequestInit) {
     const req = new Request(input, init);
 
     const { method, url, headers, mode, credentials } = req;
+    const header = {
+        ...Object.fromEntries([...headers]),
+        'Content-Type': ['HEAD', 'GET'].includes(method.toUpperCase())
+            ? 'text/plain'
+            : headers.get('Content-Type')
+    };
+    const Accept = headers.get('Accept');
 
-    const { statusCode, errMsg, header, data } = await request<ArrayBuffer>({
+    const response = await request<ArrayBuffer>({
         // @ts-ignore
         method,
         url,
-        header: Object.fromEntries([...headers]),
+        header,
         // @ts-ignore
         mode,
         credentials,
         data: await req.arrayBuffer(),
-        responseType: 'arraybuffer'
+        // @ts-expect-error https://nervjs.github.io/taro-docs/docs/apis/network/request/#datatype
+        responseType: Accept.includes('text')
+            ? 'text'
+            : Accept.includes('json')
+              ? 'json'
+              : 'arraybuffer'
     });
 
-    return new Response(data, {
-        status: statusCode,
-        statusText: errMsg,
-        headers: header
+    return new Response(response.data, {
+        status: response.statusCode,
+        statusText: response.errMsg,
+        headers: response.header
     });
 }
